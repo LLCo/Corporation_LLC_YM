22de606e1b1f113a6d36a2b06b67f9da21cec486
yangmeng6666 <37968818+yangmeng6666@users.noreply.github.com>
2018-10-16 16:11:26 +0800

    message

--- query_vec.py
+++ query_vec.py
@@ -5,22 +5,17 @@ import re
 import pandas as pd
 import time
 
-
 r1 = u'[a-zA-Z0-9’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~]+'
 path = '../data/word_vec/news_12g_baidubaike_20g_novel_90g_embedding_64.bin'
 model = gs.models.KeyedVectors.load_word2vec_format(path, binary=True)
 
 
 def get_vec(word):  # 获得词向量
-    if word in model:
+    try:
         return model.wv[word]
-    else:
+    except KeyError as e:
+        # print(e)
         return np.zeros(64)
-    # try:
-    #    return model.wv[word]
-    # except KeyError as e:
-    #    #print(e)
-    #    return np.zeros(64)
 
 
 def sen_cut(sen):  # jieba分割句子
@@ -32,39 +27,40 @@ def sen_cut(sen):  # jieba分割句子
 
 
 def get_stvec(sen):  # 获得句子向量
-    sen = re.sub(r1, '', sen).replace(' ','')
+    sen = re.sub(r1, '', sen).replace(' ', '')
     if len(sen) == 0:
         return np.zeros(64)
     word_l = sen_cut(str(sen))
     vec = []
+
     for item in word_l:
         vec.append(get_vec(item))
     vec = np.array(vec)
     return vec.sum(axis=0)
 
 
-def get_dict_vec(dic):  # 获得字典（附加信息）的句向量
+def get_dict_vec(dic):  # 获得字典的句向量
     dic = eval(dic)
     sen_l = []
     weight = []
     for item in dic.keys():
-        sen_l.append((get_stvec(item)*float(dic[item])))
+        sen_l.append((get_stvec(item) * float(dic[item])))
         weight.append(float(dic[item]))
-    length = 10-len(sen_l)
-    res = np.zeros((length,64)).tolist()
+    length = 10 - len(sen_l)
+    res = np.zeros((length, 64)).tolist()
     weight = np.array(weight)
-    #print(res,sen_l)
+    # print(res,sen_l)
     res.extend(sen_l)
     sen_l = np.array(res)
-    return np.array(sen_l/weight.sum())
+    return np.array(sen_l / weight.sum())
 
 
 def cos(vector1, vector2):
-    op7 = np.dot(vector1, vector2)/(np.linalg.norm(vector1)*(np.linalg.norm(vector2)))
+    op7 = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * (np.linalg.norm(vector2)))
     return op7
 
 
-def get_similarity(dic0, title):  # 获得字典（附加信息）与title的相似度
+def get_similarity(dic0, title):
     dic = eval(dic0)
     dic_sorted = sorted(dic.items(), key=lambda x: x[1], reverse=True)
     num = np.zeros(10)
@@ -74,25 +70,29 @@ def get_similarity(dic0, title):  # 获得字典（附加信息）与title的相
         num[i] = item[1]
     simi = np.nan_to_num(simi)
     num = np.nan_to_num(num)
-    mean = simi.mean()
-    num_toone = num/num.sum()
-    return np.hstack([simi, num, num_toone, mean])
+    mean = simi.sum() / len(dic.keys())
+    max_ = simi.max()
+    min_ = simi.min()
+    num_toone = num / num.sum()
+    sum_ = (num_toone * simi).sum()
+    return np.hstack([simi, num, num_toone, mean, max_, min_, sum_])
 
 
 def get_semantic_attr(dataframe):
     attr = []
     s = time.time()
     for index, row in dataframe.iterrows():
-        attr.append(get_similarity(row['query_prediction'], row['title_tag']))
+        attr.append(get_similarity(row['query_prediction'], row['title']))
         if index % 10000 == 0:
-           s_s = time.time()-s
-           print('###-itre', index, '-###')
-           print('time:', s_s)
-           s = time.time()
+            s_s = time.time() - s
+            print('###-itre', index, '-###')
+            print('time:', s_s)
+            s = time.time()
 
     length = len(attr[0])
-    attr_name = ['new_tag'+str(x) for x in range(length)]
+    attr_name = ['new_tag' + str(x) for x in range(length)]
     new_df = pd.DataFrame(attr, columns=attr_name)
     return new_df
+
 
 
